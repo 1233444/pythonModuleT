d3dc9b8e1c45b219f6a8ccefe3db11eb4390efd3
1233444 <1233444@gmail.com>
2021-01-03 02:48:59 -0500

    complete module taining stage one

---
+++ pyImport_Mod_PartModxx02.py
@@ -0,0 +1,38 @@
+import pyImport_Mod_PartModxx01
+import sys
+import os
+
+
+
+#print location of the standard library
+print(os.__file__)
+
+#Added path to the pythonpath envirnoment
+sys.path.append('')
+# import pyImport_Mod_PartModxx01 as mmxx
+# from pyImport_Mod_PartModxx01 import find_index, test
+# from pyImport_Mod_PartModxx01 import find_index as fixx, test as txx
+
+course = ['History','Math', 'Physics', 'CompSci']
+
+index = pyImport_Mod_PartModxx01.find_index(course, 'CompSci')
+
+# index = mmxx.find_index(course, 'Math')
+
+# index = fixx(course, 'CompSci')
+
+
+print(index)
+# print(index)
+
+
+print(dir(pyImport_Mod_PartModxx01.__name__))
+
+print(sys.path)
+
+#Using data in module depend on how you import from module
+#Case $1 if not specify on import statment
+# print(mmxx.test)
+#case $2 if already specify on import statement then can use it directly
+# print(test)
+# print(txx)
---
+++ pyMod_OS.py
@@ -0,0 +1,5 @@
+import os
+import pyqrcode
+
+# print(dir(os))
+print(dir(pyqrcode))
